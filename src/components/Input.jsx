64c219779ddb7451219428ade8925f94be2a08c4
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-05-16 20:27:27 -0300

    added responsiveness

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,5 @@
 import styles from './Input.module.css'
 
 export default function Input({type, placeholder, value, event}) {
-  return <input type={type} placeholder={placeholder} value={value} onChange={(e) => event(e)} className={styles.input} />
+  return <input type={type} inputMode='decimal' placeholder={placeholder} value={value} onChange={(e) => event(e)} className={styles.input} />
 }
